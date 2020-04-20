6c8f6c6ada4b05213bdcb0235b849d287210e13d
oliver <oliverstoreyyoung@gmail.com>
2020-04-20 09:22:31 +0100

    Updated files

--- Chrome Extension/BlockElement.js
+++ Chrome Extension/BlockElement.js
@@ -1,15 +1,27 @@
-/* var domain = new URL(window.location.href).hostname;
-chrome.storage.sync.get(["Websites"], function(result){
-    if(!(jQuery.isEmptyObject(result))){
-        if(typeof result["Websites"][domain] !== 'undefined')
-            for(var key in result["Websites"][domain]){
-                result["Websites"][domain][key].forEach(item => {
-                $(item).css("display","none");
-            });
-        }
+document.addEventListener('DOMSubtreeModified', injectCSS);
+function injectCSS(){
+    if(document.head){
+        document.removeEventListener('DOMSubtreeModified', injectCSS);
+        var domain = new URL(window.location.href).hostname;
+        chrome.storage.sync.get(["Websites"], function(result){
+            if(!(jQuery.isEmptyObject(result))){
+                if(typeof result["Websites"][domain] !== 'undefined'){
+                    var cssString = "";
+                    var style = document.createElement("style");
+                    for(var key in result["Websites"][domain]){
+                        result["Websites"][domain][key].forEach(item => {
+                            cssString += item + ", ";
+                        });
+                    }
+                    cssString = cssString.slice(0, -2) + " { display: none !important }"
+                    style.innerHTML = cssString;
+                    console.log(style);
+                    document.head.appendChild(style);
+                }
+            }
+        });
     }
-}); */
-
+};
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       if (request.reload == true)
         location.reload();   
