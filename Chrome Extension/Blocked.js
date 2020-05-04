f7baed5ac8795354eced14604fc54a91bdbb3cbf
oliver <oliverstoreyyoung@gmail.com>
2020-05-04 21:04:46 +0100

    Added context menu, storing domains in sync and blocking from sync & file

--- Chrome Extension/Blocked.js
+++ Chrome Extension/Blocked.js
@@ -1,7 +1,8 @@
-var Blocked = [
-    "*://*.lb.usemaxserver.de/*",
-    "*://*.tracking.klickthru.com/*",
-    "*://*.zmedia.com/*",
-    "*://*.zv1.november-lax.com/*",
-    "*://*.trc.taboola.com/*"
+var blocked = [
+"*://*.0001-cab8-4c8c-43de.reporo.net/*",
+"*://*.002-slq-470.mktoresp.com/*",
+"*://*.004-btr-463.mktoresp.com/*",
+"*://*.005.free-counters.co.uk/*",
+"*://*.006.free-counters.co.uk/*",
+"*://*.youtube.com/*",
 ];
--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,15 +1,76 @@
+var DomainsToBlock = [];
+
 chrome.runtime.onInstalled.addListener(function(){
     var storage = {};
     storage["Websites"] = {};
     storage["Settings"] = {"BlockType" : "Visibility"};
     storage["Settings"]["Whitelist"] = [];
+    storage["Blocked"] = [];
     chrome.storage.sync.set(storage);
+
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
-	function() {
-		return {cancel: true };
-	},
-	{urls: Blocked},
-	["blocking"]
-);
+CreateBlockList();
+//Creates the blocklist from storage & file;
+function CreateBlockList(){
+    DomainsToBlock.push("*://*.www.123123123.abcabcabc.com/*"); // need one item in there for sure
+    DomainsToBlock = DomainsToBlock.concat(blocked);
+    chrome.storage.sync.get(["Blocked"], function(result){
+
+        //Check to make sure it's run on install yet
+        if(typeof result["Blocked"] !== 'undefined'){
+            DomainsToBlock = DomainsToBlock.concat(result["Blocked"]);
+        }
+
+        //Add initial listener
+        chrome.webRequest.onBeforeRequest.addListener(
+            DomainBlock,
+            {urls: DomainsToBlock},
+            ["blocking"]
+        );
+    });
+}
+
+
+
+//Context menu for right click
+chrome.contextMenus.create({
+    title: "Block this domain", 
+    contexts:["link"], 
+    onclick: function(clickedLink){
+        //Create url format
+        var link = new String(new URL(clickedLink.linkUrl).hostname);
+        link = "*://*." + link + "/*";
+        chrome.storage.sync.get(["Blocked"], function(result){
+            if(result["Blocked"].includes(link) == false){
+                result["Blocked"].push(link);
+                chrome.storage.sync.set(result, function(){
+
+                    //Reset listeners and update list
+                    chrome.webRequest.onBeforeRequest.removeListener(DomainBlock);
+                    CreateBlockList();
+                    chrome.webRequest.onBeforeRequest.addListener(
+                        DomainBlock,
+                        {urls: DomainsToBlock},
+                        ["blocking"]
+                    );
+                    ReloadPage();  
+                });
+
+            }
+        });
+    }
+  });
+
+  
+
+//callback, needed for attatch and detatch
+function DomainBlock(){
+    return {cancel: true };
+}
+
+function ReloadPage(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {reload: true});
+    });
+}
